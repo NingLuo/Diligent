8c0982e404e817defca5bcc245db6441fd623d35
NingLuo <luo.n@husky.neu.edu>
2017-07-24 13:51:35 -0400

    Added Business Logic Layer

---
+++ Diligent.BLL/BLLBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Diligent.BLL
+{
+    public class BLLBase
+    {
+        public BLLBase()
+        {
+            ErrorList = new List<string>();
+        }
+
+        public List<string> ErrorList { get; set; }
+    }
+}
---
+++ Diligent.BLL/UserBs.cs
@@ -0,0 +1,30 @@
+﻿using Diligent.BOL;
+using Diligent.DAL;
+
+namespace Diligent.BLL
+{
+    public class UserBs : BLLBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public UserBs(DiligentContext context)
+        {
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        public bool Register(User user)
+        {
+            if (!IsValidOnRegister(user)) return false;
+
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Complete();
+
+            return true;
+        }
+
+        private bool IsValidOnRegister(User user)
+        {
+            return true;
+        }
+    }
+}
